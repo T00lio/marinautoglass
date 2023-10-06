daea6a22ce5fa956febee45c4969833f8a6ce3db
Tulio Salvatierra <salvacorp@gmail.com>
2023-10-05 20:00:44 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "../src/images/marinauto.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Marin Auto Glass Corp</p>
+        <p>Mobile & shop installation Cape Coral</p>
+        <p>Windshield replacement & repair</p>
+        <p>Tel: 786 718 2919</p>
+        <p>Email: marinautoglasscorp@gmail.com</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
