c29334b535c97da4399d70c1eb535fe5044884b4
Tulio Salvatierra <salvacorp@gmail.com>
2023-10-12 18:11:29 -0400

    Update layout and added cards with mock info

--- src/App.js
+++ src/App.js
@@ -10,13 +10,10 @@ function App() {
         <Navbar />
         <HeroSection />
 
-        <p>Marin Auto Glass Corp</p>
-        <p>Mobile & shop installation Cape Coral</p>
-        <p>Windshield replacement & repair</p>
-        <p>Tel: 786 718 2919</p>
-        <p>Email: marinautoglasscorp@gmail.com</p>
-        <p>Address: 1414 SE 10th St, Cape Coral, FL 33990</p>
-        <p>Hours: 8am-5pm</p>
+        <footer className="footer">
+          {" "}
+          Coded by <strong>Tulio Salvatierra 2023</strong>
+        </footer>
       </div>
     </>
   );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,19 +1,75 @@
 import React from "react";
-import logo from "../images/marinauto.png";
 import "../components/Hero.css";
+import marinvan from "../images/marinvan.jpg";
 
 function HeroSection() {
   return (
-    <div className="hero-section">
-      <div className="welcome-message text-center">
-        <h1 className="display-1">Marin Auto Glass</h1>
-        <h4 className="center">El mejor de Cape Coral</h4>
-        <button className="btn btn-primary mx-auto mb-5">Contactame</button>
+    <>
+      <div className="hero-section">
+        <div className="container">
+          <div className="row">
+            <div className="welcome-message col">
+              <h3 className="title">
+                Welcome to Marin Auto Glass Cape Coral, Fl
+              </h3>
+              <p></p>
+              <p className="text">
+                Mobile & shop installation Cape Coral: Windshield replacement/
+                repair. Also we fix window motors & locks.<br></br> Tel: 786 718
+                2919 Email: marinautoglasscorp@gmail.com <br></br>Hours: 8am-5pm
+                <br></br> Address: 1414 SE 10th St, Cape Coral, FL 33990{" "}
+              </p>
+              <button className="btn btn-danger mx-auto mb-5 d-flex">
+                Contactame
+              </button>
+            </div>
+          </div>
+          <br></br>
+          <section className="row">
+            <h1
+              className="display-1 text-center"
+              style={{ color: "white", textAlign: "center", margin: "auto" }}
+            >
+              About Us
+            </h1>
+            <div class="container mt-5">
+              <div class="row">
+                <div class="col">
+                  <div class="card" style={{ width: "18rem" }}>
+                    <img src={marinvan} class="card-img-top" alt="..." />
+                    <div class="card-body">
+                      <p class="card-text"></p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card" style={{ width: "18rem" }}>
+                    <img src={marinvan} class="card-img-top" alt="..." />
+                    <div class="card-body">
+                      <p class="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card" style={{ width: "18rem" }}>
+                    <img src={marinvan} class="card-img-top" alt="..." />
+                    <div class="card-body">
+                      <p class="card-text">
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
-      <div className="logo-container">
-        <img src={logo} className="App-logo" alt="logo" />
-      </div>
-    </div>
+    </>
   );
 }
 
